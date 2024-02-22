a4ebae4d5528fdd551ef3da00db896c5ab8fc11a
paolodevprojects <paolo.dev.projects@gmail.com>
2024-02-21 22:03:31 -0500

    [FIX] routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,8 +2,8 @@ import { Route, createBrowserRouter, createRoutesFromElements } from "react-rout
 import Root from "./routes/root";
 import NotFoundPage from "./pages/NotFoundPage";
 import HomePage from "./pages/HomePage";
-import {action as loginAction} from "./routes/login";
-import {action as registerAction} from "./routes/register";
+import { action as loginAction } from "./routes/login";
+import { action as registerAction } from "./routes/register";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import Dashboard from "./routes/dashboard";
@@ -15,18 +15,18 @@ import SettingsPage from "./pages/dashboard/SettingsPage";
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route>
-            <Route path="/" element={<Root/>}>
-                <Route index element={<HomePage/>}/>
-                <Route path="/login" element={<LoginPage/>} action={loginAction}/>
-                <Route path="/register" element={<RegisterPage/>} action={registerAction}/>
+            <Route path="/" element={<Root />}>
+                <Route index element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} action={loginAction} />
+                <Route path="/register" element={<RegisterPage />} action={registerAction} />
             </Route>
-            <Route path="/dashboard" element={<Dashboard/>}>
-                <Route index element={<DashboardPage/>}/>
-                <Route path="/dashboard/contact" element={<ContactPage/>}/>
-                <Route path="/dashboard/report" element={<ReportPage/>}/>
-                <Route path="/dashboard/settings" element={<SettingsPage/>}/>
+            <Route path="/dashboard" element={<Dashboard />}>
+                <Route index element={<DashboardPage />} />
+                <Route path="contact" element={<ContactPage />} />
+                <Route path="report" element={<ReportPage />} />
+                <Route path="settings" element={<SettingsPage />} />
             </Route>
-            <Route path="*" element={<NotFoundPage />} />
+            <Route path="/*" element={<NotFoundPage />} />
         </Route>
     )
 )
